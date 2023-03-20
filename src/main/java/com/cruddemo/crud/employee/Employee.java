47d4c5cf3c9e4f41d30b271a4308d107c7c304b8
ahmadabdeen2 <ahmadabdin3@hotmail.com>
2023-03-20 11:50:38 +0300

    first

---
+++ src/main/java/com/cruddemo/crud/employee/Employee.java
@@ -0,0 +1,31 @@
+package com.cruddemo.crud.employee;
+
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+
+    @Column(name = "email")
+    private String email;
+    
+}
